b997815db0279c684ec31f46570daa47f8d0688b
Gaurav Balyan <balyang@HCL.COM>
2019-06-11 17:29:46 +0530

    Changes made to product NTA

--- azure/nta/templates/installation_script.ps1
+++ azure/nta/templates/installation_script.ps1
@@ -1,7 +1,6 @@
 param
 (
-	[string]$typeOfInstallation,
-	[string]$productsToInstall,
+	[string]$silentConfigUri,
 	[string]$dbServerName, 
 	[string]$databaseName, 
 	[string]$dbUserName, 
@@ -10,76 +9,36 @@ param
 
 Start-Transcript -Path C:\postinstall.Log
 
-if($typeOfInstallation -eq "false" -OR $typeOfInstallation -eq "False" -OR $typeOfInstallation -eq "FALSE") {
-	[bool]$isStandard = $false
-}
-else {
-	[bool]$isStandard = $true
-}
+write-host "downloading silent installer config file from $silentConfigUri"; [datetime]::Now
+$filePath = "C:\Windows\Temp\silentconfig.xml"
+Invoke-WebRequest $silentConfigUri -OutFile $filePath
+write-host "download completed: $filePath"; [datetime]::Now
 
-write-host ' installing NuGet module....'; [datetime]::Now
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-write-host ' installed NuGet module....'; [datetime]::Now
+$installer_path = "https://downloads.solarwinds.com/solarwinds/OnlineInstallers/RTM/NTA/Solarwinds-Orion-NTA.exe"
 
-write-host ' installing azure module....'; [datetime]::Now
-Install-Module -Name Azure,AzureRM -force
-write-host ' installed azure module....'; [datetime]::Now
-
-$s_name = "vinayblob1"
-$pass = "MOSlYQq3cMy+ZsZtqUmaHBL3gZ2PQshjmyKimPLBupDYrq9EWnDcujXNY3XyPUUf3g/EcFLMPnZbdt4vGzZ5DA=="
-$ctx = new-azurestoragecontext -StorageAccountName $s_name -StorageAccountKey $pass
-
-write-host 'coping text file from azure blob....'; [datetime]::Now
-Get-AzureStorageBlobContent -Blob installers_list.json -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx
-
-if($isStandard)
-{
-	Get-AzureStorageBlobContent -Blob standard.xml -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx
-}
-else
-{
-	Get-AzureStorageBlobContent -Blob express.xml -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx	
-}
-#Get-AzureStorageBlobContent -Blob sqldetail.txt  -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx
-#Remove-AzureStorageBlob -Blob sqldetail.txt -Container vinay-storage-account-container -Context $ctx
-write-host ' copied text file from azure blob....'; [datetime]::Now
-
-write-host 'reading installer name from json file....'; [datetime]::Now
-$installers_list_object = Get-Content 'C:\Windows\Temp\installers_list.json' | Out-String | ConvertFrom-Json
-$installer_name = $installers_list_object.$productsToInstall
-write-host 'got the installer name from json file....'; [datetime]::Now
-
-write-host ' copying solarwindinstaller  from azure blob....'; [datetime]::Now
-Get-AzureStorageBlobContent -Blob $installer_name  -Container vinay-storage-account-container -Destination C:\Windows\Temp\ -Context $ctx
-write-host ' copied solarwindinstaller  from azure blob....'; [datetime]::Now
-
-$filePath = 'C:\Windows\Temp\express.xml'
-if($isStandard)
-{
-	$filePath = 'C:\Windows\Temp\standard.xml'
-}
+write-host "downloading solarwind online installer from $installer_path"; [datetime]::Now
+$installer_name  = "Solarwinds-Orion-NTA.exe"
+Invoke-WebRequest $installer_path -OutFile "C:\Windows\Temp\$installer_name"
+write-host "download completed: C:\Windows\Temp\$installer_name"; [datetime]::Now
 
 $xml=New-Object XML
 $xml.Load($filePath)
 
-$node=$xml.SilentConfig.InstallerConfiguration
-$node.ProductsToInstall=$productsToInstall 
-
-if($isStandard)
+if($xml.SilentConfig.Host.Info.Database)
 {
 	$node=$xml.SilentConfig.Host.Info.Database	
-	$node.ServerName=$dbServerName
+	$node.ServerName=$dbServerName+$node.ServerName
 	$node.DatabaseName=$databaseName
 	$node.User=$dbUserName    
 	$node.UserPassword=$dbPassword
+	
+	$xml.Save($filePath)
 }
-
-$xml.Save($filePath)
 
 New-Item C:\Windows\Temp\installer.ps1 -ItemType file
 Add-Content 'C:\Windows\Temp\installer.ps1' .\$installer_name" /s /ConfigFile=""$filePath"""
-write-host ' starting installation solarwindinstaller....'; [datetime]::Now
 
+write-host ' starting installation solarwindinstaller....'; [datetime]::Now
 cd "C:\Windows\Temp"
 .\installer.ps1
 write-host ' installation started solarwindinstaller....'; [datetime]::Now
@@ -89,47 +48,26 @@ while(1)
 {
 	$Solarwinds = Get-Process $process_name -ErrorAction SilentlyContinue
 	if ($Solarwinds) {
-	  Sleep 5
-	  Remove-Variable Solarwinds
-	  continue;
+		Sleep 5
+		Remove-Variable Solarwinds
+		continue;
 	}
 	else {
-		write-host "process completed"
+		write-host "process completed"; [datetime]::Now
 		Remove-Variable Solarwinds
-	    break;
+		break;
 	}
 }
 
 write-host ' Deleting the files created in installation process'; [datetime]::Now
 
-$installer_type_file = "C:\Windows\Temp\"+$typeOfInstallation+".xml"
-if (Test-Path $installer_type_file) 
-{
-  Remove-Item $installer_type_file
-  write-host ' Installer_type file deleted '; [datetime]::Now
-}
-
-$installer_list_file = "C:\Windows\Temp\installers_list.json"
-if (Test-Path $installer_list_file ) 
-{
-  Remove-Item $installer_list_file 
-  write-host ' installer list file deleted'; [datetime]::Now
-}
-
 $installer_file = "C:\Windows\Temp\installer.ps1"
 if (Test-Path $installer_file) 
 {
-  Remove-Item $installer_file
-  write-host 'silent installer file deleted'; [datetime]::Now
-}
-
-$installer_exe_file = "C:\Windows\Temp\"+$installer_name
-if (Test-Path $installer_exe_file) 
-{
-  Remove-Item $installer_exe_file
-  write-host ' installer setup file deleted'; [datetime]::Now 
+	Remove-Item $installer_file
+	write-host 'silent installer file deleted'; [datetime]::Now
 }
 
 write-host 'Files deleted which has been created in installation process'; [datetime]::Now 
-#exit 1
+
 Stop-Transcript
