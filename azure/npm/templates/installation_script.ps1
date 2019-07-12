7be053814e959b25ea408efcfe7486a4a5174711
vnootan <vnootan@gmail.com>
2019-07-12 16:20:52 +0530

    Formatted the code

--- azure/npm/templates/installation_script.ps1
+++ azure/npm/templates/installation_script.ps1
@@ -1,13 +1,13 @@
 param
 (
-	[string]$silentConfigUri,
-	[string]$installerUri,
-	[string]$dbServerName, 
-	[string]$databaseName, 
-	[string]$dbUserName, 
-	[string]$dbPassword,
-	[string]$appUserPassword,
-	[string]$vmName
+    [string]$silentConfigUri,
+    [string]$installerUri,
+    [string]$dbServerName, 
+    [string]$databaseName, 
+    [string]$dbUserName, 
+    [string]$dbPassword,
+    [string]$appUserPassword,
+    [string]$vmName
 )
 
 Start-Transcript -Path C:\postinstall.Log
@@ -20,30 +20,28 @@ write-host "download completed: $configfilePath"; [datetime]::Now
 
 #download the installer from Artifacts
 write-host "downloading installer from $installerUri"; [datetime]::Now
-$installer_name  = "Solarwinds-Orion-NPM.exe"
+$installer_name = "Solarwinds-Orion-NPM.exe"
 Invoke-WebRequest $installerUri -OutFile "C:\Windows\Temp\$installer_name"
 write-host "download completed: $installerfilePath"; [datetime]::Now
 
 #update DB details
-$xml=New-Object XML
+$xml = New-Object XML
 $xml.Load($configfilePath)
 
-$node=$xml.SilentConfig.InstallerConfiguration
-$node.WebConsolePassword=$appUserPassword
+$node = $xml.SilentConfig.InstallerConfiguration
+$node.WebConsolePassword = $appUserPassword
 
-if($xml.SilentConfig.Host.Info.Database)
-{
-	$dbnode=$xml.SilentConfig.Host.Info.Database	
-	$dbnode.ServerName=$dbServerName
-	$dbnode.DatabaseName=$databaseName
-	$dbnode.User=$dbUserName    
-	$dbnode.UserPassword=$dbPassword
-	$dbnode.AccountPassword=$dbPassword
+if ($xml.SilentConfig.Host.Info.Database) {
+    $dbnode = $xml.SilentConfig.Host.Info.Database	
+    $dbnode.ServerName = $dbServerName
+    $dbnode.DatabaseName = $databaseName
+    $dbnode.User = $dbUserName    
+    $dbnode.UserPassword = $dbPassword
+    $dbnode.AccountPassword = $dbPassword
 }
-if($xml.SilentConfig.Host.Info.Website)
-{
-	$nodeWebsite = $xml.SilentConfig.Host.Info.Website
-	$nodeWebsite.CertificateResolvableCN = $vmName
+if ($xml.SilentConfig.Host.Info.Website) {
+    $nodeWebsite = $xml.SilentConfig.Host.Info.Website
+    $nodeWebsite.CertificateResolvableCN = $vmName
 }
 
 $xml.Save($configfilePath)
@@ -59,30 +57,28 @@ Set-Location "C:\Windows\Temp"
 write-host ' installation started solarwindinstaller....'; [datetime]::Now
 
 #check for if installation status
-$process_name = $installer_name.Substring(0,$installer_name.LastIndexOf('.'))
-while(1)
-{
-	$Solarwinds = Get-Process $process_name -ErrorAction SilentlyContinue
-	if ($Solarwinds) {
-		Start-Sleep 5
-		Remove-Variable Solarwinds
-		continue;
-	}
-	else {
-		write-host "process completed"; [datetime]::Now
-		Remove-Variable Solarwinds
-		break;
-	}
+$process_name = $installer_name.Substring(0, $installer_name.LastIndexOf('.'))
+while (1) {
+    $Solarwinds = Get-Process $process_name -ErrorAction SilentlyContinue
+    if ($Solarwinds) {
+        Start-Sleep 5
+        Remove-Variable Solarwinds
+        continue;
+    }
+    else {
+        write-host "process completed"; [datetime]::Now
+        Remove-Variable Solarwinds
+        break;
+    }
 }
 
 #delete files created in installation process
 write-host ' Deleting the files created in installation process'; [datetime]::Now
 
 $installer_file = "C:\Windows\Temp\installer.ps1"
-if (Test-Path $installer_file) 
-{
-	Remove-Item $installer_file
-	write-host 'silent installer file deleted'; [datetime]::Now
+if (Test-Path $installer_file) {
+    Remove-Item $installer_file
+    write-host 'silent installer file deleted'; [datetime]::Now
 }
 
 write-host 'Files deleted which has been created in installation process'; [datetime]::Now 
